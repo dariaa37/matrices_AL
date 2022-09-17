c4e2c241326d13369141dfda026b06dd1edf4678
Karla Díaz <diazaguilarkarla8@gmail.com>
2022-09-16 20:58:42 -0500

    docs: variable changes r(rows) to f(fila)

--- matrix_KDA.py
+++ matrix_KDA.py
@@ -15,9 +15,9 @@ matrix = [
 size = len(matrix[0])
 
 # Define row (fila)
-r1 = matrix[0]
-r2 = matrix[1]
-r3 = matrix[2]
+f1 = matrix[0]
+f2 = matrix[1]
+f3 = matrix[2]
 
 # From INT to FLOAT
 for i in range(len(matrix)):
@@ -29,77 +29,77 @@ print(" > Vuelve el sistema una matriz < ".center(50,"-") +"\n")
 theMatrix(matrix)
 
 # CERO 1
-coeficiente = r2[0]*(-1)/r1[0]       # Divide 2/4 to got coef. and modify file 2
+coeficiente = f2[0]*(-1)/f1[0]       # Divide 2/4 to got coef. and modify file 2
 print(f"· Coeficiente: {coeficiente}") # PROFF
 
 for colum in range(0, size):
-    r2[colum] += r1[colum] * coeficiente
+    f2[colum] += f1[colum] * coeficiente
 
 print(" CERO 1: Fila 2 - Fila 1 * coeficiente ".center(50,"·")+"\n")
 theMatrix(matrix)
 
 # CERO 2
-coeficiente = r3[0]*(-1)/r1[0]       # Divide -1/4 to got coef. and modify file 2
+coeficiente = f3[0]*(-1)/f1[0]       # Divide -1/4 to got coef. and modify file 2
 print(f"· Coeficiente: {coeficiente}") # PROFF
 
 for colum in range(0, size):
-    r3[colum] += r1[colum] * coeficiente
+    f3[colum] += f1[colum] * coeficiente
 
 print(" CERO 2: Fila 3 - Fila 1 * coeficiente ".center(50,"·")+"\n")
 theMatrix(matrix)
 
 # CERO 3
-coeficiente = r3[1]*(-1)/r2[1]
+coeficiente = f3[1]*(-1)/f2[1]
 print(f"· Coeficiente: {coeficiente}") # PROFF
 
 for colum in range(0,size):
-    r3[colum] += r2[colum] * coeficiente
+    f3[colum] += f2[colum] * coeficiente
 
 print(" CERO 3: Fila 3 - Fila 2 * coeficiente ".center(50,"·")+"\n")
 theMatrix(matrix)
 
 # CERO 4
-coeficiente = r1[2]*(-1)/r3[2]
+coeficiente = f1[2]*(-1)/f3[2]
 print(f"· Coeficiente: {coeficiente}") # PROFF
 
 for colum in range(0,size):
-    r1[colum] += r3[colum] * coeficiente
+    f1[colum] += f3[colum] * coeficiente
 
 print(" CERO 4: Fila 1 - Fila 3 * coeficiente ".center(50,"·")+"\n")
 theMatrix(matrix)
 
 # CERO 5
-coeficiente = r2[2]*(-1)/r3[2]
+coeficiente = f2[2]*(-1)/f3[2]
 print(f"· Coeficiente: {coeficiente}") # PROFF
 
 for colum in range(0, size):
-    r2[colum] += r3[colum] * coeficiente
+    f2[colum] += f3[colum] * coeficiente
 
 print(" CERO 5: Fila 2 - Fila 3 * coeficiente ".center(50,"·")+"\n")
 theMatrix(matrix)
 
 # CERO 6
-coeficiente = r1[1]*(-1)/r2[1]
+coeficiente = f1[1]*(-1)/f[1]
 print(f"· Coeficiente: {coeficiente}") # PROFF
 
 for colum in range(0, size):
-    r1[colum] += r2[colum] * coeficiente
+    f1[colum] += f2[colum] * coeficiente
 
 print(" CERO 6: Fila 1 - Fila 2 * coeficiente ".center(50,"·")+"\n")
 theMatrix(matrix)
 
 # Guardar denominadores
-deno1 = r1[0]
-deno2 = r2[1]
-deno3 = r3[2]
+deno1 = f1[0]
+deno2 = f2[1]
+deno3 = f3[2]
 
 # Diagonal de 1
 for colum in range(0, size):
-    r1[colum] /= deno1
-    r2[colum] /= deno2
-    r3[colum] /= deno3
+    f1[colum] /= deno1
+    f2[colum] /= deno2
+    f3[colum] /= deno3
 
 print(" MATRIZ COMPLETADA ".center(50,"=") +"\n")
 theMatrix(matrix)
 
-print(f"Result ->\t X = {r1[3]}\t Y = {r2[3]}\t Z = {r3[3]}")
+print(f"Result ->\t X = {f1[3]}\t Y = {f2[3]}\t Z = {f3[3]}")
